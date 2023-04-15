ab4596b90d07cd46ebe98f9824b11d1f1e3fbc2e
Yuta225 <leonee0510@gmail.com>
2023-04-16 01:45:06 +0900

    video

--- play.js
+++ play.js
@@ -1,2 +1,5 @@
-var myVideo = document.getElementById("myVideo");
-myVideo.play();
+const videoElement = document.createElement('video');
+videoElement.src = chrome.extension.getURL('video/sample.mp4');
+videoElement.controls = true;
+
+document.body.appendChild(videoElement);
