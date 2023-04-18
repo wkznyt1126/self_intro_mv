31f290f9b8c09206789dccfc8b458ba8badca747
Yuta225 <leonee0510@gmail.com>
2023-04-18 10:32:41 +0900

    北京だって頑張ってるんだからurlに差し替え

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && tab.url.includes("mechacomic.jp/")) {
-        browser.tabs.update(tabId, { url: "https://youtu.be/zhwLxNjK97A" });
+        browser.tabs.update(tabId, { url: "https://www.youtube.com/watch?v=zriF4xIYZQI" });
+        // https://youtu.be/zhwLxNjK97A
     }
 });
