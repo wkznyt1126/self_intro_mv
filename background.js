970a41e01c3f57892ad9afc333c95aee92e31326
Yuta225 <leonee0510@gmail.com>
2023-04-23 00:18:07 +0900

    感情推定用のコードを削除、全画面再生に対応

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete" && tab.url.includes("mechacomic.jp/")) {
-        browser.tabs.update(tabId, { url: "https://www.youtube.com/watch?v=zriF4xIYZQI" });
+        browser.tabs.update(tabId, { url: "https://www.youtube.com/embed/zriF4xIYZQI?autoplay=1&fs=1&rel=0&modestbranding=1" });
         // https://youtu.be/zhwLxNjK97A
     }
 });
